ebc5bd8516d9f7758d5526a01d49bc42198f8c66
Vadim <polish4uck@gmail.com>
2020-06-18 21:16:59 +0300

    first commit

---
+++ Gulpfile.js
@@ -0,0 +1,16 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const sourcemaps = require('gulp-sourcemaps');
+const watch = require('gulp-watch');
+
+gulp.task('sass-compile', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sourcemaps.init())
+    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest('./css'));
+})
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', gulp.series('sass-compile'))
+});
